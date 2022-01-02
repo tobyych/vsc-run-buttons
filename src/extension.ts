6d41c83447a13ad1acbade824bbd27954c239363
toby <toby@tobys-MacBook-Air.local>
2022-01-03 00:56:10 +0800

    change to use debugging api

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,46 @@ export async function activate(context: vscode.ExtensionContext) {
 
     console.log('Congratulations, your extension "vsc-task-buttons" is now active!');
 
-    const mapping: Array<[string, string]> = [
-        ['TaskButtons.run', 'workbench.action.tasks.runTask'],
-        ['TaskButtons.rerun', 'workbench.action.tasks.reRunTask'],
-    ]
+    let currentWorkspace: vscode.WorkspaceFolder;
+    let uri;
+    if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
+        currentWorkspace = vscode.workspace.workspaceFolders[0];
+        uri = currentWorkspace.uri;
+    }
 
-    mapping.forEach(([buttonName, action]) => {
-        let disposable = vscode.commands.registerCommand(buttonName, () => {
-            vscode.commands.executeCommand(action);
+    const config: Array<any>  = vscode.workspace.getConfiguration(
+        'launch',
+        uri
+    ).configurations;
+
+    let lastSelection: string;
+
+    const runCommand = vscode.commands.registerCommand('RunButtons.run', () => {
+        const quickPick = vscode.window.createQuickPick();
+        quickPick.items = config.map(x => ({'label': x.name}));
+        quickPick.onDidChangeSelection(selection => {
+            if (selection[0]) {
+                quickPick.hide();
+                if (!lastSelection) {
+                    vscode.commands.executeCommand('setContext', 'hasLastSelection', true);
+                }
+                lastSelection = selection[0].label;
+                vscode.debug.startDebugging(currentWorkspace, selection[0].label);
+            }
         });
-        context.subscriptions.push(disposable);
+        quickPick.onDidHide(() => quickPick.dispose());
+        quickPick.show();
     });
+
+    context.subscriptions.push(runCommand);
+    
+    const rerunCommand = vscode.commands.registerCommand('RunButtons.rerun', () => {
+        if (lastSelection) {
+            vscode.debug.startDebugging(currentWorkspace, lastSelection);
+        }
+    });
+
+    context.subscriptions.push(rerunCommand);
 
 }
 
