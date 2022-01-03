8b8fefc66fd3d76128f3dceaa180a5229cc1f334
Toby <tobyych@gmail.com>
2022-01-03 11:51:29 +0800

    updated to use run submenu for run buttons

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,52 @@
 import * as vscode from 'vscode';
 
-// activate is called when VS Code detected there is a .vscode/tasks.json
+// activate is called when VS Code detected there is a .vscode/launch.json
 export async function activate(context: vscode.ExtensionContext) {
 
     console.log('Congratulations, your extension "vsc-task-buttons" is now active!');
 
     let currentWorkspace: vscode.WorkspaceFolder;
-    let uri;
     if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
         currentWorkspace = vscode.workspace.workspaceFolders[0];
-        uri = currentWorkspace.uri;
     }
 
-    const config: Array<any>  = vscode.workspace.getConfiguration(
-        'launch',
-        uri
-    ).configurations;
-
-    let lastSelection: string;
-
-    const runCommand = vscode.commands.registerCommand('RunButtons.run', () => {
+    let currentSelection: string;
+    context.subscriptions.push(vscode.commands.registerCommand('RunButtons.list', () => {
+        const config: Array<any>  = vscode.workspace.getConfiguration(
+            'launch',
+            currentWorkspace.uri
+        ).configurations;
+        if (config.length === 0) {
+            vscode.window.showErrorMessage("No run configuration in `launch.json` file!");
+            return;
+        }
         const quickPick = vscode.window.createQuickPick();
         quickPick.items = config.map(x => ({'label': x.name}));
         quickPick.onDidChangeSelection(selection => {
             if (selection[0]) {
                 quickPick.hide();
-                if (!lastSelection) {
-                    vscode.commands.executeCommand('setContext', 'hasLastSelection', true);
-                }
-                lastSelection = selection[0].label;
-                vscode.debug.startDebugging(currentWorkspace, selection[0].label);
+                currentSelection = selection[0].label;
             }
         });
         quickPick.onDidHide(() => quickPick.dispose());
         quickPick.show();
-    });
+    }));
 
-    context.subscriptions.push(runCommand);
-    
-    const rerunCommand = vscode.commands.registerCommand('RunButtons.rerun', () => {
-        if (lastSelection) {
-            vscode.debug.startDebugging(currentWorkspace, lastSelection);
+    context.subscriptions.push(vscode.commands.registerCommand('RunButtons.run', () => {
+        if (currentSelection) {
+            vscode.debug.startDebugging(currentWorkspace, currentSelection, {'noDebug': true});
+        } else {
+            vscode.window.showErrorMessage("No selected run configuration. Please use the menu button on the right to select one.");
         }
-    });
+    }));
 
-    context.subscriptions.push(rerunCommand);
+    context.subscriptions.push(vscode.commands.registerCommand('RunButtons.debug', () => {
+        if (currentSelection) {
+            vscode.debug.startDebugging(currentWorkspace, currentSelection);
+        } else {
+            vscode.window.showErrorMessage("No selected run configuration. Please use the menu button on the right to select one.");
+        }
+    }));
 
 }
 
